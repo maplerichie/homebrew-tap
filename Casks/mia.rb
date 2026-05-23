e2721084e56e5e5359fb52992a40d396ad7d2274
LikKee Richie <10757316+maplerichie@users.noreply.github.com>
2026-05-23 15:25:57 +0800

    Create mia.rb

---
+++ Casks/mia.rb
@@ -0,0 +1,11 @@
+cask "mia" do
+  version "0.1.2"
+  sha256 "b93df2f85475f1d53fd47723615bcb3289f309d209c557ce420d9f677aa7826e"
+
+  url "https://github.com/maplerichie/Mia/releases/download/beta-test/Mia.zip"
+  name "Mia"
+  desc "A native macOS menu-bar app for tracking subscriptions, spend, and provider usage."
+  homepage "https://x.com/maplerichie"
+
+  app "Mia.app"
+end
